9f49f6dd6df65ca429a2093a7c7c1c3bbe4d9e45
Oisinmarron <74601763+Oisinmarron@users.noreply.github.com>
2020-12-04 17:13:15 +0000

    Finished Game

--- FlappyBirdClone/GameScene.swift
+++ FlappyBirdClone/GameScene.swift
@@ -17,10 +17,6 @@ struct PhysicsCategory{
 }
 
 class GameScene: SKScene, SKPhysicsContactDelegate {
-    /*
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
-    */
     
     var Character = SKSpriteNode()
     var Grass = [SKSpriteNode]()
@@ -122,8 +118,8 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         
         
         // CHARACTER
-        Character = SKSpriteNode(imageNamed: "Character")
-        Character.size = CGSize(width: self.frame.width/2 * 0.3, height: self.frame.height/2 * 0.25)
+        Character = SKSpriteNode(imageNamed: "Fly")
+        Character.size = CGSize(width: 200, height: 130)
         Character.position = CGPoint(x: 0 - self.frame.width/4, y: 0)
         
         Character.physicsBody = SKPhysicsBody(rectangleOf: Character.size)
@@ -182,6 +178,8 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
             
             if died == false{
                 died = true
+                let moveTexture = SKTexture(imageNamed: "Dead")
+                Character.texture = moveTexture
                 createBtn()
             }
         }
@@ -198,6 +196,8 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
             
             if died == false{
                 died = true
+                let moveTexture = SKTexture(imageNamed: "Dead")
+                Character.texture = moveTexture
                 createBtn()
             }
         }
@@ -207,7 +207,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         
         let scoreNode = SKSpriteNode()
         
-        scoreNode.size = CGSize(width: 3, height: Character.size.height * 2)
+        scoreNode.size = CGSize(width: 3, height: Character.size.height * 2.5)
         scoreNode.position = CGPoint(x: self.frame.width, y: 0)
         scoreNode.physicsBody = SKPhysicsBody(rectangleOf: scoreNode.size)
         scoreNode.physicsBody?.affectedByGravity = false
@@ -215,7 +215,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         scoreNode.physicsBody?.categoryBitMask = PhysicsCategory.Score
         scoreNode.physicsBody?.collisionBitMask = 0
         scoreNode.physicsBody?.contactTestBitMask = PhysicsCategory.Character
-        scoreNode.color = SKColor.blue
+        // scoreNode.color = SKColor.blue
         
         let randomPosition = CGFloat.random(min: -self.frame.height/5, max: self.frame.height/5)
         
@@ -232,9 +232,9 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         topWall.setScale(0.75)
         btmWall.setScale(0.75)
         
-        topWall.position = CGPoint(x: self.frame.width, y: topWall.size.height/2 + Character.size.height)
+        topWall.position = CGPoint(x: self.frame.width, y: topWall.size.height/2 + Character.size.height * 1.15)
         
-        btmWall.position = CGPoint(x: self.frame.width, y: -btmWall.size.height/2 - Character.size.height)
+        btmWall.position = CGPoint(x: self.frame.width, y: -btmWall.size.height/2 - Character.size.height * 1.15)
         
         
         topWall.physicsBody = SKPhysicsBody(rectangleOf: topWall.size)
@@ -267,50 +267,12 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         self.addChild(wallPair)
     }
     
-    /*
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
-    }
     
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
-    }
-*/
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?){
         if gameStarted == false{
             
             gameStarted = true
+            
             Character.physicsBody?.affectedByGravity = true
             
             let spawn = SKAction.run({
@@ -320,7 +282,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
                 
             })
             
-            let delay = SKAction.wait(forDuration: 2.0)
+            let delay = SKAction.wait(forDuration: 2.5)
             let spawnDelay = SKAction.sequence([spawn, delay])
             let spawnDelayForever = SKAction.repeatForever(spawnDelay)
             self.run(spawnDelayForever)
@@ -331,7 +293,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
             moveAndRemove = SKAction.sequence([movePillars, removePillars])
             
             Character.physicsBody?.velocity = CGVector(dx: 0, dy: 0)
-            Character.physicsBody?.applyImpulse(CGVector(dx: 0, dy: 400))
+            Character.physicsBody?.applyImpulse(CGVector(dx: 0, dy: 500))
         }
         else{
             if died == true{
@@ -339,7 +301,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
             }
             else{
                 Character.physicsBody?.velocity = CGVector(dx: 0, dy: 0)
-                Character.physicsBody?.applyImpulse(CGVector(dx: 0, dy: 400))
+                Character.physicsBody?.applyImpulse(CGVector(dx: 0, dy: 500))
             }
         }
         
@@ -355,26 +317,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         }
     }
         
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?){
-       /* print("touch ended")
-        super.touchesEnded(touches, with: event)*/
-        //for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-/*
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
-    }
-    
-    
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-*/
+ 
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
         
